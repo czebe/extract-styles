717122e36b514a35070a8bdac023e0cc84bc1d63
czebem <marton.czebe@possible.com>
2017-11-13 17:21:27 +0100

    Parser dev

--- src/lib/scssParser.js
+++ src/lib/scssParser.js
@@ -1,54 +1,70 @@
 import postcss from 'postcss';
 import postcssScss from 'postcss-scss';
 
-const isMarked = (node, mark) => {
-	const nextNode = node.next();
-	const markPattern = new RegExp(mark, 'i');
-	if (nextNode && nextNode.type === 'comment') {
-		nextNode.remove();
-		if (markPattern.test(nextNode.text)) return true;
-	} else if (node.type === 'comment') {
-		node.remove();
-		if (markPattern.test(node.text)) return true;
+const DEFAULT_MARK = '@theme';
+
+export const markPattern = (mark = DEFAULT_MARK) => new RegExp(`^(\/[\/|\*].*?${mark})([^\*]*)`, 'im');
+
+const isMarked = (node, pattern) => {
+
+
+	console.log(node.type);
+
+	if (node.type === 'comment') {
+		// console.log(node.text.match(markPattern));
+
+		if (pattern.test(node.text)) {
+			return true;
+		}
+	} else {
+		const nextNode = node.next();
+		console.log('SSSS', nextNode.text.match(pattern));
+
+		if (nextNode && nextNode.type === 'comment') {
+
+			nextNode.remove();
+			if (pattern.test(nextNode.text)) return true;
+		}
 	}
 
 	return false;
 };
 
-export const parse = (scss, mark = '@theme') => {
+export const parse = (scss, mark = DEFAULT_MARK) => {
 	let parsed;
-	const markPattern = new RegExp(`\/\/\.*?${mark}`, 'igm');
-	if (scss && markPattern.test(scss)) {
 
+	if (scss && scss.indexOf(mark) >= 0) {
+		const pattern = markPattern(mark);
 		const root = postcss().process(scss, {syntax: postcssScss}).root;
 
 		root.walkDecls(node => {
-			if (!isMarked(node, mark)) {
+			if (!isMarked(node, pattern)) {
 				node.remove();
 			}
 		});
 
-		root.walkAtRules(node => {
-			if (node.name === 'include' && !isMarked(node, mark)) {
+		// root.walkAtRules(node => {
+		// 	if (node.name === 'include' && !isMarked(node, markPattern)) {
+		// 		node.remove();
+		// 	}
+		// });
+		//
+		// // Remove empty at-rules (@if, @for etc).
+		// root.walkAtRules(node => {
+		// 	if (node.name !== 'include' && (!node.nodes || !node.nodes.length)) {
+		// 		node.remove();
+		// 	}
+		// });
+		//
+
+		// Remove comments
+		root.walkComments(node => {
+			if (!isMarked(node, pattern)) {
 				node.remove();
 			}
 		});
 
-		root.walkAtRules(node => {
-			if ((node.name !== 'include') && (!node.nodes || !node.nodes.length)) {
-				node.remove();
-			} else {
-				node.remove();
-			}
-		});
-
-		root.walkRules(node => {
-			if (!node.nodes || !node.nodes.length) {
-				node.remove();
-			}
-		});
-
-		parsed = root.toString().trim();
+		parsed = root.toString().trim().replace(/^\s*[\r\n]/gm, ''); // Remove empty lines
 	}
 
 	return parsed;
--- test/scssParser.spec.js
+++ test/scssParser.spec.js
@@ -2,28 +2,66 @@ import {expect} from 'chai';
 import {underline, bgBlue, bold, whiteBright} from 'chalk';
 
 import {readFile} from '../src/lib/io';
-import {parse} from '../src/lib/scssParser';
+import {parse, markPattern} from '../src/lib/scssParser';
 
 describe(bgBlue(whiteBright('scssParser')), () => {
 
+
+	describe(underline(bold('markPattern()')), () => {
+
+		it('should detect correct mark pattern and ignore incorrect ones', () => {
+			const mark = '@theme';
+			const pattern = markPattern();
+
+			// Correct marks
+			expect(('// @theme').match(pattern)).to.have.lengthOf(3);
+			expect(('//@theme').match(pattern)).to.have.lengthOf(3);
+			expect(('/* @theme */').match(pattern)).to.have.lengthOf(3);
+			expect(('/*@theme*/').match(pattern)).to.have.lengthOf(3);
+			expect(('// @theme').match(pattern)).to.have.lengthOf(3);
+
+			// Incorrect marks
+			expect(('// theme').match(pattern)).to.be.equal(null);
+			expect(('// @ theme').match(pattern)).to.be.equal(null);
+
+			// Extended marks
+			expect(('// @theme: foo').match(pattern)[2]).to.have.lengthOf(5);
+			expect(('/* @theme: foo */').match(pattern)[2]).to.have.lengthOf(6);
+		});
+
+	});
+
 	describe(underline(bold('parse()')), () => {
 
-		let scssA, scssB;
+		let scssA, scssB, scssC, scssD;
 
 		before(async () => {
 			scssA =  await readFile('test/fixtures/a.scss');
 			scssB =  await readFile('test/fixtures/b.scss');
+			scssC =  await readFile('test/fixtures/c.scss');
+			scssD =  await readFile('test/fixtures/d.scss');
 		});
 
 		it('should return undefined when theme mark is not found', () => {
 			expect(parse()).to.be.equal(undefined);
 			expect(parse('')).to.be.equal(undefined);
-			expect(parse('.class { color: red; } // @custom-mark')).to.be.equal(undefined);
+			expect(parse('.class { color: red; // @custom-mark }')).to.be.equal(undefined);
 		});
 
 		it('should return parsed SCSS when theme mark is found', () => {
-			expect(parse('.class { color: red; } // @theme')).to.not.be.equal(undefined);
-			expect(parse('.class { color: red; } // @custom-mark', '@custom-mark')).to.not.be.equal(undefined);
+
+			/*
+			expect(parse(`
+				.class {
+					color: red; // @theme
+				}
+			`)).to.not.be.equal(undefined);
+			expect(parse(`
+				.class {
+					color: red; // @custom-mark
+				}
+			`, '@custom-mark')).to.not.be.equal(undefined);
+			*/
 		});
 
 		it('should return parsed SCSS for multiline strings', () => {
@@ -36,12 +74,29 @@ describe(bgBlue(whiteBright('scssParser')), () => {
 			expect(scss[1]).to.include('background-color: white;');
 		});
 
-		it.only('should detect theme mark in nested SCSS', () => {
-			// const scss = parse(scssB).split('\n');
-			const scss = parse(scssB);
-			console.log(scss);
-			// expect(scss.length).to.be.equal(3);
-			// expect(scss[1]).to.include('background-color: white;');
+		it('should detect theme mark in nested SCSS', () => {
+			const scss = parse(scssB).split('\n');
+			expect(scss.length).to.be.equal(14);
+			expect(scss[4]).to.include('background-color: green;');
+			expect(scss[6]).to.include('background-color: blue;');
+			expect(scss[12]).to.include('color: #ffff00;');
+		});
+
+		it('should keep @at-rule declarations when marked and remove if not', () => {
+			const scss = parse(scssC).split('\n');
+			expect(scss.length).to.be.equal(11);
+			expect(scss[1]).to.include('@include size(100%);');
+			expect(scss[2]).to.include('@for $i from 1 through 3 {');
+			expect(scss[7]).to.include('@if 5 < 3 {');
+		});
+
+		it.only('should remove unmarked comments and keep marked ones', () => {
+
+			console.log(parse(scssD));
+
+			// const scss = parse(scssD).split('\n');
+			// expect(scss.length).to.be.equal(11);
+			// expect(scss[1]).to.include('@include size(100%);');
 		});
 
 	});
